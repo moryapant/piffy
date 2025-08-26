69563c5372c526dd62aac0681ec9cffc7699a088
moreshwar <morya123@gmaail.com>
2025-08-26 17:18:07 +0530

    release

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -14,7 +14,7 @@ defineProps({
 </script>
 
 <template>
-  <Head title="Home" />
+  <Head title="Home - Fappify" />
 
   <MainLayout>
     <div class="py-4 sm:py-6 md:py-8 mx-auto space-y-4 sm:space-y-6 px-2 sm:px-4 md:px-6 lg:px-8">
